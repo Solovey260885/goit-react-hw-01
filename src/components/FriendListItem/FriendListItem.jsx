9926d4e62bb7ec5b44c66609e22587a20c4f121e
Alexandr Solovey <158730378+Solovey260885@users.noreply.github.com>
2024-08-08 12:23:39 +0300

    add task-2

---
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -0,0 +1,17 @@
+import clsx from 'clsx';
+import css from './FriendListItem.module.css';
+
+export default function FriendListItem({ item: { avatar, name, isOnline } }) {
+  const statusClsx = clsx(
+    css.statusText,
+    isOnline ? css.isActive : css.isRetired
+  );
+
+  return (
+    <div className={css.item}>
+      <img src={avatar} alt={name} width="48" className={css.img} />
+      <p className={css.title}>{name}</p>
+      <p className={statusClsx}>{isOnline ? 'Online' : 'Offline'}</p>
+    </div>
+  );
+}
